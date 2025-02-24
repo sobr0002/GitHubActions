855e76dbface8365118d9aa144ec033234eeca84
Sofie Broman <sofierytter@hotmail.com>
2025-02-24 11:01:09 +0100

    nyt projekt

---
+++ src/main/java/org/example/hellogithubactions/HelloGitHubActionsApplication.java
@@ -0,0 +1,13 @@
+package org.example.hellogithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGitHubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGitHubActionsApplication.class, args);
+    }
+
+}
